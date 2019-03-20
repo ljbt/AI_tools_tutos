a824b7cabeafa4d2c17203cc4c268335e773e721
rafoutek <desmond1297@gmail.com>
2019-03-20 14:53:01 +0100

    delete some files

--- tuto_opencv/td_opencv_8/additional_functions.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include "opencv2/imgcodecs.hpp"
-#include <opencv2/highgui.hpp>
-#include <opencv2/ml.hpp>
-using namespace cv;
-using namespace cv::ml;
-using namespace std;
